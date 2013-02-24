e1c0a6962cef354c41bd5b6c3be4381f47731000
Samuel Liard <samuel@apptom.fr>
2013-02-24 22:46:48 +0100

    add apptom java sqk

--- jetty/recipes/default.rb
+++ jetty/recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "apptom::java"
+include_recipe "java::apptom"
 
 case node["platform"]
 when "centos","redhat","fedora"
