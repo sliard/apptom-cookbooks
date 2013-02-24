54db537dd6d1cc12fb41eeb1b5b34d187fbe00db
Samuel Liard <samuel@apptom.fr>
2013-02-24 23:08:11 +0100

    add apptom java sqk

--- java/recipes/default.rb
+++ java/recipes/default.rb
@@ -1,33 +1,12 @@
-#
-# Author:: Seth Chisamore (<schisamo@opscode.com>)
-# Cookbook Name:: java
-# Recipe:: default
-#
-# Copyright 2008-2011, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-if node['platform_family'] == "windows"
-  include_recipe "java::windows"
-else
-  include_recipe "java::#{node['java']['install_flavor']}"
-end
+raise "Unknown java version #{node.java.default_version}, in #{node.java.versions.keys.join(" ")}" unless node.java.versions.keys.include? node.java.default_version
 
-# Purge the deprecated Sun Java packages if remove_deprecated_packages is true
-%w[sun-java6-jdk sun-java6-bin sun-java6-jre].each do |pkg|
-  package pkg do
-    action :purge
-    only_if { node['java']['remove_deprecated_packages'] }
-  end
+include_recipe "java::java_#{node.java.default_version}"
+
+execute "set default java version" do
+  command <<-EOF
+  update-alternatives --set java /usr/lib/jvm/#{node.java.versions[node.java.default_version]}/bin/java &&
+  update-alternatives --set javac /usr/lib/jvm/#{node.java.versions[node.java.default_version]}/bin/javac
+  EOF
+  not_if "readlink /etc/alternatives/java | grep #{node.java.versions[node.java.default_version]}"
 end
